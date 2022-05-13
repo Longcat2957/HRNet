d8ae2dc3bbdd69d33e667b9b4a21d4b7d753d565
Longcat2957 <91525066+Longcat2957@users.noreply.github.com>
2022-05-13 16:25:40 +0900

    v1.0

--- utils/aihub_kpop_dataset.py
+++ utils/aihub_kpop_dataset.py
@@ -2,14 +2,17 @@ import os
 import torch
 import pandas as pd
 import ast
-from torch import Tensor
+import numpy as np
+import math
 
-
+from PIL import Image
+from scipy import ndimage
+from torch import Tensor, nn
 from pathlib import Path
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from torchvision.io import read_image   # Reads a JPEG or PNG image into a 3 dimensional RGB or grayscale Tensor. Optionally converts the image to the desired format. The values of the output tensor are uint8 in [0, 255].
+from torchvision import transforms
 from torchvision.transforms.functional import crop, resize
-from target_generators import ScaleAwareHeatmapGenerator
 
 # Directory
 current_path = Path(os.path.dirname(os.path.abspath(__file__))) # ../HRNET/utils 의 절대 경로
@@ -19,24 +22,23 @@ annotation_dir = data_path / 'annotation'
 image_dir = data_path / 'image'
 annotation_file_path = annotation_dir / 'annotation.csv'
 
-def kpopimgT(img, bbox:list):
-    l, t, w, h = bbox
-    img = crop(img, top=t, left=l, height=h, width=w)
-    img = resize(img, (256, 256))  # resize to 256 x 192
-    return img
-    
 
-def kpoplabelT(joints, bbox:list):
+
+def kpoplabelT(joints, bbox:list, sigma:int=1):
     l, t, w, h = bbox
     empty_list = []
     for i in range(29):
         x, y, v = joints[3*i], joints[3*i+1], joints[3*i+2]
-        x = (x - l)/w
-        y = (y - t)/h
-        empty_list.append((y, x, v))
-    # BBOX의 상대적인 위치
-    # visibility = 0, 1, 2
+        rx, ry = (x-l)/w, (y-t)/h
 
+        zero = np.zeros((64, 64))
+        if v > 0:
+            for a in range(64):
+                for b in range(64):
+                    zero[a][b] = math.exp(-((64 * rx - b)**2+(64 * ry - a)**2)/(2 * sigma**2))
+        empty_list.append(zero)
+    # BBOX의 절대적인 위치를 리턴
+    # visibility = 0, 1, 2
     return torch.Tensor(empty_list)
 
 
@@ -45,7 +47,10 @@ class KpopImageDatasetwT(Dataset):
     def __init__(self, annotations_file=annotation_file_path):
         self.img_labels = pd.read_csv(annotations_file)
         self.num_joints = 29
-        self.label_transformer = ScaleAwareHeatmapGenerator(output_res=64, num_joints=self.num_joints)
+        self.label_transform = torch.nn.UpsamplingNearest2d(size=(64,64))
+        self.img_T = transforms.Compose([transforms.Resize((256, 256)),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         
     def __len__(self):
         return len(self.img_labels)
@@ -53,16 +58,16 @@ class KpopImageDatasetwT(Dataset):
     def __getitem__(self, idx):
         line = self.img_labels.loc[idx]
         img_path = line[2]
-        image = read_image(img_path)
+        image = Image.open(img_path)
         label = ast.literal_eval(line[5])
         
         # for transform
         bbox = ast.literal_eval(line[3])
         # l, t, w, h = bbox
 
-        image = kpopimgT(image, bbox)
+        image = self.img_T(image)
         label = kpoplabelT(label, bbox)
-        label = self.label_transformer(label)
+
         
         return image, label
 
